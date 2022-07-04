065336a5c6afc091dd12e39c6ac63ab554bb618f
cyber <cyber@localhost.localdomain>
2022-07-04 11:05:00 -0400

    updating

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import setuptools
+import shutil
 
 if __name__ == "__main__":
     setuptools.setup()
+    shutil.copyfile("src/flake/flake.ini", "/etc/flake.ini")
--- src/flake/filters/config.py
+++ src/flake/filters/config.py
@@ -2,18 +2,18 @@ import configparser
 import os
 import sys
 
-config = configparser.ConfigParser()
-config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'filter.ini')
-config.read(config_path)
+configp = configparser.ConfigParser()
+config_path = '/etc/flake.ini'
+configp.read(config_path)
 
 # check if the path is to a valid file
 if not os.path.isfile(config_path):
     print("Invalid configuration path provided.")
-    raise BadConfigError # not a standard python exception
+    sys.exit()
 
 def initFromConfig(param):
-    for section in config.sections():
-        if config.has_option(section, param):
-            return config[section][param]
-    print("Error initializing " + str(param) + " from config. Parameter not found.")
+    for section in configp.sections():
+        if configp.has_option(section, param):
+            return configp[section][param]
+    print("Error initializing " + str(param) + "from config. Parameter not found.")
     sys.exit()
